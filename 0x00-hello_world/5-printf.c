af267d96da1900879b5b9ce39d0dda134d859f63
Samuel <samuelmaglodji@gmail.com>
2022-09-08 16:39:40 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print a string
+ *
+ * Description: using the main function
+ * this program display an output with printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
