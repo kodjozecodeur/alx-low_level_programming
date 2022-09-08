5869bcdd0d7afdf113405388b2ca476d0ef8ac09
Samuel <samuelmaglodji@gmail.com>
2022-09-08 16:23:57 -0700

    show me something

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main-print the string to the function
+ *
+ * Description:using the main fuction
+ * to prints something without printf
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
